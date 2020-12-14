45ad160ba1fd8b22ffdec9a991d825a819b3cfd7
Ibrahima DANSOKO <ibrahima.Dansoko@outlook.com>
2020-12-14 16:03:05 +0100

    fix error typeorm

--- src/modules/structure/structure.module.ts
+++ src/modules/structure/structure.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { StructureController } from './structure.controller';
 import { StructureService } from './structure.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Structure } from '../../entities';
 
 @Module({
   controllers: [StructureController],
+  imports: [
+    TypeOrmModule.forFeature([Structure]),
+  ],
   providers: [StructureService],
   exports: [StructureModule],
 })
